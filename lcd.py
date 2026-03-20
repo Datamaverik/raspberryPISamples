33c3be1d3242d9f8249cbd768b2c985f736339b0
Datamaverik <verma.pranshu160705@gmail.com>
2026-03-20 10:20:06 +0530

    fix: lcd code

--- lcd.py
+++ lcd.py
@@ -1,35 +1,50 @@
 # sudo raspi-config
-# enable i2c in the interfacing option
+# Interface Options → I2C → Enable
 # sudo reboot
-# i2cdetect -l
 # i2cdetect -y 1
 # sudo pi3 install rpi_lcd
 # sudo find /usr/local -name rpi_lcd 2> /dev/null -> gives the location of the library
 # go to that location and edit the address if needed
 
-from signal import signal, SIGTERM, SIGHUP, pause
-from rpi_lcd import LCD
+# sudo apt update
+# sudo apt install python3-rpi.gpio python3-smbus i2c-tools
+# sudo apt install python3-rplcd
+# sudo apt install python3-gpiozero
 
-lcd = LCD()
+from RPLCD.gpio import CharLCD
+import RPi.GPIO as GPIO
+import time
 
-def safe_exit(signum, frame):
-    exit(1)
-    
-signal(SIGTERM, safe_exit)
-signal(SIGHUP, safe_exit)
+GPIO.setmode(GPIO.BCM)
+
+lcd = CharLCD(
+    pin_rs=25,
+    pin_e=24,
+    pins_data=[23, 17, 27, 22],
+    numbering_mode=GPIO.BCM
+)
 
 try:
-    lcd.text("Hello,", 1)
-    lcd.text("Raspberry Pi", 2)
-    pause()
+    lcd.clear()
+    lcd.write_string("Hello Shreyash!")
+
+    time.sleep(2)
+
+    lcd.clear()
+    lcd.write_string("Parallel LCD")
 
 except KeyboardInterrupt:
-    pass
-
-finally:
     lcd.clear()
     
-# GND -> GND (PIN6)
-# VCC -> 5V (PIN2)
-# SDA -> PIN3
-# SCL -> PIN5
+# VSS	GND
+# VDD	5V
+# V0	Potentiometer (contrast)
+# RS	GPIO25
+# RW	GND
+# E	GPIO24
+# D4	GPIO23
+# D5	GPIO17
+# D6	GPIO27
+# D7	GPIO22
+# A (LED+)	5V
+# K (LED-)	GND
